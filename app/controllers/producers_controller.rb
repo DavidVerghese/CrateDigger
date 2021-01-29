1f9cd8aa94636521facec03f7f59a419cc0138d3
David Verghese <dj.verghese@gmail.com>
2021-01-29 15:51:08 -0500

    took out some commented out code

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,18 +1,12 @@
 class ProducersController < ApplicationController
-    # GET /songs
     def index
       @producers = Producer.all
-  
       render json: @producers
     end
-  
-    # GET /producers/1
     def show
       @producer = Producer.new(producer_params)
       render json: @producer
     end
-  
-    # POST /producers
     def create
       @producer = Producer.new(producer_params)
   
@@ -22,27 +16,19 @@ class ProducersController < ApplicationController
         render json: @producer.errors, status: :unprocessable_entity
       end
     end
-  
-    # PATCH/PUT /producers/1
     def update
       @producer = Producer.find(params[:id])
       @producer.update(producer_params)
       render json: @producer
     end
-  
-    # DELETE /producers/1
     def destroy
       @producer = Producer.find(params[:id])
       @producer.destroy
     end
-  
     private
-      # Use callbacks to share common setup or constraints between actions.
       def set_producer
         @producer = Producer.find(params[:id])
       end
-  
-      # Only allow a trusted parameter "white list" through.
       def producer_params
         params.require(:producer).permit(:name, :genre, :lifetime, :location)
       end
