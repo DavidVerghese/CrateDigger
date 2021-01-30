d262546bbb9ad08c0209bec106862299d5995eca
David Verghese <dj.verghese@gmail.com>
2021-01-29 22:07:15 -0500

    installed gemfile and got data from rails server into react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Route, Link, Switch, useHistory } from 'react-router-dom';
+import { getAll, deleteSongsample, postSongsample, putSongsample } from './services/songssamples';
 
 function App() {
+  const [allSongssamples, setAllSongssamples] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    // 3. call your function that sets all dogs in state
+    fetchSongssamples();
+  }, [])
+
+  // 2. make a function to get all dogs and then set state
+
+  const fetchSongssamples = async () => {
+    const songssamples = await getAll();
+    setAllSongssamples(songssamples);
+    console.log(songssamples);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hi!
     </div>
   );
 }
