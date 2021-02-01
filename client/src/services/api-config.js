2681228e342f852618685846cca8b540cdadeb14
David Verghese <dj.verghese@gmail.com>
2021-02-01 16:51:24 -0500

    prepping for deployment

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+// const api = axios.create({
+//   baseURL: "http://localhost:3000"
+
+// })
+
+const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://git.heroku.com/cratedigger2-api.git/' : 'http://localhost:3000' 
+
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: baseUrl
 })
 
 export default api;
