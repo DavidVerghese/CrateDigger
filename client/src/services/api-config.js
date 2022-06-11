03b5470da0d5b8b13f51df84dd9375123e294db1
David Verghese <dj.verghese@gmail.com>
2022-06-11 19:16:27 -0400

    Changed url in api config

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 // })
 
-const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://cratedigger2-api.herokuapp.com/' : 'http://localhost:3000' 
+const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://cratedigger.surge.sh/' : 'http://localhost:3000' 
 
 const api = axios.create({
   baseURL: baseUrl
